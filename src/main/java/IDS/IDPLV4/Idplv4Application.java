2a74e648340f0c5fa09ca39673845c7566840810
EgaIDS <129152495+EgaIDS@users.noreply.github.com>
2023-06-23 20:28:50 +0700

    init

---
+++ src/main/java/IDS/IDPLV4/Idplv4Application.java
@@ -0,0 +1,13 @@
+package IDS.IDPLV4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idplv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idplv4Application.class, args);
+	}
+
+}
---
+++ src/test/java/IDS/IDPLV4/Idplv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package IDS.IDPLV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idplv4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
